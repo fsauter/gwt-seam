15d236192c071adfa097d6da35c6b067d2899946
FloOnWeb@googlemail.com <FloOnWeb@googlemail.com@b6b75efd-a3e3-aaa5-ce3f-e17a7f57449c>
2011-03-17 21:35:00 +0000

    Removed comment...

--- src/com/gwtplatform/dispatch/server/seam/DispatchConfiguration.java
+++ src/com/gwtplatform/dispatch/server/seam/DispatchConfiguration.java
@@ -2,10 +2,6 @@ package com.gwtplatform.dispatch.server.seam;
 
 /**
  * The dispatch configuration of GWTP Seam.
- * <p>
- * IMPORTANT: Do not forget to annotate you implementation with
- * {@link org.jboss.seam.annotations.Startup}
- * </p>
  * 
  * @author Florian Sauter
  */
