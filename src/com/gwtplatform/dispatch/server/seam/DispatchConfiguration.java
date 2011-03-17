2a9c3b56dd4e0aac960ea21bb14f2bb017ebe839
FloOnWeb@googlemail.com <FloOnWeb@googlemail.com@b6b75efd-a3e3-aaa5-ce3f-e17a7f57449c>
2011-03-17 21:34:31 +0000

    Removed getEndPoint() method and unused constants.

--- src/com/gwtplatform/dispatch/server/seam/DispatchConfiguration.java
+++ src/com/gwtplatform/dispatch/server/seam/DispatchConfiguration.java
@@ -1,8 +1,5 @@
 package com.gwtplatform.dispatch.server.seam;
 
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Scope;
-
 /**
  * The dispatch configuration of GWTP Seam.
  * <p>
@@ -12,12 +9,6 @@ import org.jboss.seam.annotations.Scope;
  * 
  * @author Florian Sauter
  */
-@Scope(ScopeType.APPLICATION)
 public interface DispatchConfiguration {
-	
-	public static final String DEFAULT_ENDPOINT = "seam/resource/gwtp";
-	
-	String getEndPoint();
-
 	void configureHandlers(HandlerRegistry handlerRegistry);
 }
