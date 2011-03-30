9d07dbd6610d3c99f7c3d051a952d6cacd125ed2
FloOnWeb@googlemail.com <FloOnWeb@googlemail.com@b6b75efd-a3e3-aaa5-ce3f-e17a7f57449c>
2011-03-30 12:33:14 +0000

    Minor improvements.

--- trunk/src/com/gwtplatform/dispatch/server/seam/DispatchConfiguration.java
+++ trunk/src/com/gwtplatform/dispatch/server/seam/DispatchConfiguration.java
@@ -1,5 +1,7 @@
 package com.gwtplatform.dispatch.server.seam;
 
+import com.google.gwt.user.server.rpc.seam.GWTService;
+
 /**
  * The dispatch configuration of GWTP Seam.
  * 
@@ -7,6 +9,7 @@ package com.gwtplatform.dispatch.server.seam;
  */
 public interface DispatchConfiguration {
 	
+	public static final String DEFAULT_ENDPOINT = "seam/resource" + GWTService.DEFAULT_RESOURCE_PATH;
 	public static final String COMPONENT_NAME = "gwtpDispatchConfiguration";
 	
 	void configureHandlers(HandlerRegistry handlerRegistry);
